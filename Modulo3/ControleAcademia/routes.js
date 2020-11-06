87a6f88f2b41542f914ea007f0b4829c62f539ef
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-06 07:12:15 -0300

    add route create

--- Modulo3/ControleAcademia/routes.js
+++ Modulo3/ControleAcademia/routes.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const routes = express.Router()
+const express = require('express');
+const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.redirect('instructors')
@@ -11,14 +11,24 @@ routes.get('/instructors', (req, res) => {
 
 routes.get('/instructors/create', (req, res) => {
     return res.render('instructor/create')
-})
+});
 
 routes.post('/instructors', (req, res) => {
+    let keys = Object.keys(req.body);
+
+    for(key of keys) {
+
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields!')
+        }
+    }
+
     return res.send(req.body)
+    
 })
 
 routes.get('/members', (req, res) => {
     return res.send('page members')
 });
 
-module.exports = routes
+module.exports = routes;

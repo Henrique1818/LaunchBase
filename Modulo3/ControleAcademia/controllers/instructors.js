380274aef24c242316297203637c3bcf029bd575
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-08 18:13:36 -0300

    add data

--- Modulo3/ControleAcademia/controllers/instructors.js
+++ Modulo3/ControleAcademia/controllers/instructors.js
@@ -11,16 +11,28 @@ module.exports = {
             }
         }
 
-        req.body.birth = Date.parse(req.body.birth);
-        req.body.created_at = Date.now();
+        let { avatar_url, name, birth, gender, services } = req.body;
 
-        data.instructors.push(req.body);
+        birth = Date.parse(birth);
+        const id = Number(data.instructors.length + 1);
+        const created_at = Date.now();
 
-        fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
-            if(err) return res.send({ error: 'Write file error!'});
-            
-            return res.redirect("instructors");
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services,
+            created_at
         });
+
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+            if (err) return res.send('Write file error!')
+            
+            return res.redirect("/instructors");
+        });
+
         
         // return res.send(req.body)
     }

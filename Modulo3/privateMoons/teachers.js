b458db1a33832f48691810783f62b9a30640f16c
Henrique1818 <luiz.henrique01827@gmail.com>
2020-11-09 19:38:50 -0300

    challenge 4-3 solved, teacher registration form and route

--- Modulo3/privateMoons/teachers.js
+++ Modulo3/privateMoons/teachers.js
@@ -1,5 +1,37 @@
+const fs = require('fs');
+const data = require('./data.json');
+
 module.exports = {
     create(req, res) {
-        return res.send('ok')
+        let { avatar_url, name, birth, education_level, class_type, occupation } = req.body;
+
+        const keys = Object.keys(req.body);
+
+        for(key of keys) {
+            if(req.body[key] == "") return res.send('Please, fill all fields!');
+        }
+
+        let id = Number(data.teacher.length + 1);
+        const created_at = Date.now();
+        birth = Date.parse(birth);
+
+        data.teacher.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            education_level,
+            class_type,
+            occupation,
+            created_at
+        });
+
+        fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+            if(err) {
+                return res.send('Write file error!');
+            }
+
+            return res.redirect('/teachers');
+        });
     }
 }

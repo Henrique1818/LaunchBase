d2ae88d0db5548bace851f6302fe1f712ef2262c
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-11 19:22:35 -0300

    Organizando rotas criando rota do show

--- Modulo3/privateMoons/src/routes.js
+++ Modulo3/privateMoons/src/routes.js
@@ -6,18 +6,15 @@ const routes = express.Router();
 routes.get('/', (req, res) => {
     return res.redirect('/teachers');
 });
-
 routes.get('/teachers', (req, res) => {
     return res.render('teachers/index');
 });
-
 routes.get('/teachers/create', (req, res) => {
     return res.render('teachers/create');
 });
-
 routes.get('/teachers/show/:id', teachers.show);
-
 routes.post('/teachers', teachers.create);
+
 
 routes.get('/students', (req, res) => {
     return res.send('Page Students');

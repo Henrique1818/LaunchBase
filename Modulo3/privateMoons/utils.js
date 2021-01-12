f63e5a83f1bd14d2c087d6b6356f07977b4094f3
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-11 21:33:42 -0300

    criando a logica do graduation

--- Modulo3/privateMoons/utils.js
+++ Modulo3/privateMoons/utils.js
@@ -9,5 +9,21 @@ module.exports = {
         if(month < 0 || month == 0 && today.getDate() < birthDay.getDate()) return age -= 1;
 
         return age;
+    },
+    graduation(levelGraduation) {
+        let education = '';
+        
+        switch(levelGraduation) {
+            case 'EMC':
+                return education = 'Ensino Médio - Completo';
+            case 'ESC':
+                return education = 'Ensino Superior - Completo';
+            case 'masters':
+                return education = 'Mestrado';
+            case 'doctorate':
+                return education = 'Doutorado'
+        }
+        
+        return education;
     }
 }

bd2c1bd3e1c42098ffe74a75625f7c5e0177bbc8
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-13 18:56:51 -0300

    criando o metodo de date para manipular a data no formato yyyy-mm-dd

--- Modulo3/privateMoons/utils.js
+++ Modulo3/privateMoons/utils.js
@@ -25,5 +25,14 @@ module.exports = {
         }
         
         return education;
+    },
+    date(timestamp) {
+        const date = new Date(timestamp);
+
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        return `${year}-${month}-${day}`;
     }
 }

7358d88926e13cee510f640e414ad90235bf9289
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-16 19:08:48 -0300

    Configurando para usar o Method-Override

--- Modulo3/privateMoons/src/app.js
+++ Modulo3/privateMoons/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('src/public'));
+app.use(methodOverride('_method'));
 app.use(routes);
 
 app.set('view engine', 'njk');

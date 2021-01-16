746cf4d76412efb330f8da62df0539475f47b9d7
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-16 19:20:35 -0300

    Update

--- Modulo3/privateMoons/src/public/utils/button.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu')
-
-document.querySelector('button').addEventListener('click', () => {
-    if(menu.classList.contains('active')) {
-        menu.classList.remove('active')
-    } else {
-        menu.classList.add('active')
-    }
-})

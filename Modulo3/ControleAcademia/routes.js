a2b64dd71404ce80a828c2a0a2c6a11d520bc861
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-16 11:31:39 -0300

    Configurando a rota de edição

--- Modulo3/ControleAcademia/routes.js
+++ Modulo3/ControleAcademia/routes.js
@@ -6,20 +6,16 @@ const routes = express.Router();
 routes.get('/', (req, res) => {
     return res.redirect('instructors')
 });
-
 routes.get('/instructors', (req, res) => {
     return res.render('instructor/index')
 });
-
 routes.get('/instructors/create', (req, res) => {
     return res.render('instructor/create')
 });
-
-routes.get('/instructors/:id/edit', instructors.edit);
-
 routes.get('/instructors/:id', instructors.show);
-
+routes.get('/instructors/:id/edit', instructors.edit);
 routes.post('/instructors', instructors.create);
+routes.put('/instructors', instructors.put);
 
 routes.get('/members', (req, res) => {
     return res.send('page members')

16ba2a0251572637c1c765a557339f808e4ed882
Henrique1818 <luiz.henrique01827@gmail.com>
2021-01-16 11:58:31 -0300

    Criando o metodo que retorna a data em dd/mm/yyyy

--- Modulo3/ControleAcademia/utils/date.js
+++ Modulo3/ControleAcademia/utils/date.js
@@ -20,5 +20,14 @@ module.exports = {
         const day = `0${date.getUTCDate()}`.slice(-2)
 
         return `${year}-${month}-${day}`
+    },
+    ptBR(timestamp) {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return `${day}/${month}/${year}`
     }
 }

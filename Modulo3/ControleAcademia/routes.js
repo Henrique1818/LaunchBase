f5e89e7d94523e2595505d0d30ec0beb9d021a0e
Henrique1818 <luiz.henrique01827@gmail.com>
2021-03-30 15:24:35 -0300

    adicioando rotas dos members

--- Modulo3/ControleAcademia/routes.js
+++ Modulo3/ControleAcademia/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const instructors = require('./controllers/instructors');
+const members = require('./controllers/members');
 
 const routes = express.Router();
 
@@ -16,8 +17,14 @@ routes.post('/instructors', instructors.create);
 routes.put('/instructors', instructors.put);
 routes.delete('/instructors', instructors.delete);
 
-routes.get('/members', (req, res) => {
-    return res.send('page members')
+routes.get('/members', members.index);
+routes.get('/members/create', (req, res) => {
+    return res.render('instructor/create')
 });
+routes.get('/members/:id', members.show);
+routes.get('/members/:id/edit', members.edit);
+routes.post('/members', members.create);
+routes.put('/members', members.put);
+routes.delete('/members', members.delete);
 
 module.exports = routes;

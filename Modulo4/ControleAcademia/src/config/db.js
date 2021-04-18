c0fbe92014a8923acc8d007807a51a369a9c2b56
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-18 18:54:34 -0300

    :tada: criando conexao com o banco

---
+++ Modulo4/ControleAcademia/src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+    user: "launchbase",
+    password: "launchbase",
+    host: "localhost",
+    port: 5432,
+    database: "gym_manager"    
+});

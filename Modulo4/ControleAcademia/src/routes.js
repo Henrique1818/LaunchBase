4705e157ab3edfb1f0e85cf9e3cb5316dc805de2
Henrique1818 <luiz.henrique01827@gmail.com>
2021-04-18 18:54:08 -0300

    refatorando as rotas e o server

---
+++ Modulo4/ControleAcademia/src/routes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const instructors = require('./app/controllers/instructors');
+const members = require('./app/controllers/members');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    return res.redirect('instructors')
+});
+routes.get('/instructors', instructors.index);
+routes.get('/instructors/create', (req, res) => {
+    return res.render('instructor/create')
+});
+routes.get('/instructors/:id', instructors.show);
+routes.get('/instructors/:id/edit', instructors.edit);
+routes.post('/instructors', instructors.create);
+routes.put('/instructors', instructors.put);
+routes.delete('/instructors', instructors.delete);
+
+routes.get('/members', members.index);
+routes.get('/members/create', (req, res) => {
+    return res.render('member/create')
+});
+routes.get('/members/:id', members.show);
+routes.get('/members/:id/edit', members.edit);
+routes.post('/members', members.create);
+routes.put('/members', members.put);
+routes.delete('/members', members.delete);
+
+module.exports = routes;
